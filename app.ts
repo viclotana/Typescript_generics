1de76f0e65106085a9e1fb6abfe84212f88a40c1
mac <mac@macs-MacBook-Pro.local>
2019-03-30 16:53:20 +0100

    updating app

--- app.ts
+++ app.ts
@@ -1,33 +1,10 @@
-//console.log("hello worl");
 
-// class Collection{
-//     private _things:any[];
+interface NameArgs {
+    length: number;
+}
 
-//     constructor(){
-//         this._things = [];
-//     }
-
-//     add<A>(something:A):void{
-//         this._things.push(something);
-//     }
-
-//     get<B>(index:number):B{
-//         return this._things[index];
-//     }
-// }
-
-// let Strin = new Collection();
-// Strin.add<string>('hello'); 
-// Strin.add('world');
-
-// interface NameArgs {
-//     length: number;
-// }
-
-// function printName <T extends NameArgs>(arg: T){
-//     console.log(arg.length);
-//     return arg;
-// }
-// printName({length: 3, value: 3});
-
-// // console.log(Stringss.get<string>(0).length);
+function printName <T extends NameArgs>(arg: T){
+    console.log(arg.length);
+    return arg;
+}
+printName({length: 3, value: 3});
